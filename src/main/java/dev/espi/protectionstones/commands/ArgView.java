c69edf86f78a0d58a00206653dfe356233152cd3
Mykhailo Alipa <6442572+strobil@users.noreply.github.com>
2026-08-06 15:21:02 +0200

    Fix region view particle size

--- src/main/java/dev/espi/protectionstones/commands/ArgView.java
+++ src/main/java/dev/espi/protectionstones/commands/ArgView.java
@@ -143,7 +143,7 @@ public class ArgView implements PSCommandArg {
 
     private static boolean handlePurpleParticle(Player p, Location l) {
         if (p.getLocation().distance(l) > PARTICLE_VIEW_DISTANCE_LIMIT || Math.abs(l.getY()-p.getLocation().getY()) > 30) return false;
-        ParticlesUtil.persistRedstoneParticle(p, l, new Particle.DustOptions(Color.fromRGB(255, 0, 255), 10), 30);
+        ParticlesUtil.persistRedstoneParticle(p, l, new Particle.DustOptions(Color.fromRGB(255, 0, 255), 4), 30);
         return true;
     }
 }
